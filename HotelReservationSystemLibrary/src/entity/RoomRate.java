1b71ab063b499f99b989a216cc25cc07634d594d
Matthea Loo <>
2018-10-27 03:16:03 +0800

    done with entities and enums

--- HotelReservationSystemLibrary/src/entity/RoomRate.java
+++ HotelReservationSystemLibrary/src/entity/RoomRate.java
@@ -5,11 +5,20 @@
  */
 package entity;
 
+import enums.RateTypeEnum;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 /**
  *
@@ -20,32 +29,128 @@ public class RoomRate implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomRateId;
+    @Enumerated(EnumType.STRING)
+    private RateTypeEnum rateType;
+    @Column(nullable = false, precision = 18, scale = 2)
+    private BigDecimal ratePerNight;
+    @Column(nullable = false)
+    private Date startDate;
+    @Column(nullable = false)
+    private Date endDate;
+    private Boolean isUsed;
+      private Boolean isEnabled;
+      private Boolean forPartner;
+      
+@ManyToMany(mappedBy = "roomRates")
+private List<RoomType> roomTypes;
 
-    public Long getId() {
-        return id;
+    public RoomRate() {
+        roomTypes = new ArrayList<>();
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public RoomRate(Long roomRateId, RateTypeEnum rateType, BigDecimal ratePerNight, Date startDate, Date endDate, Boolean isUsed, Boolean isEnabled, Boolean forPartner) {
+        this.roomRateId = roomRateId;
+        this.rateType = rateType;
+        this.ratePerNight = ratePerNight;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isUsed = isUsed;
+        this.isEnabled = isEnabled;
+        this.forPartner = forPartner;
+    }
+
+    public RateTypeEnum getRateType() {
+        return rateType;
+    }
+
+    public void setRateType(RateTypeEnum rateType) {
+        this.rateType = rateType;
+    }
+
+    public BigDecimal getRatePerNight() {
+        return ratePerNight;
+    }
+
+    public void setRatePerNight(BigDecimal ratePerNight) {
+        this.ratePerNight = ratePerNight;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Boolean getIsUsed() {
+        return isUsed;
+    }
+
+    public void setIsUsed(Boolean isUsed) {
+        this.isUsed = isUsed;
+    }
+
+    public Boolean getIsEnabled() {
+        return isEnabled;
+    }
+
+    public void setIsEnabled(Boolean isEnabled) {
+        this.isEnabled = isEnabled;
+    }
+
+    public Boolean getForPartner() {
+        return forPartner;
+    }
+
+    public void setForPartner(Boolean forPartner) {
+        this.forPartner = forPartner;
+    }
+
+    public List<RoomType> getRoomTypes() {
+        return roomTypes;
+    }
+
+    public void setRoomTypes(List<RoomType> roomTypes) {
+        this.roomTypes = roomTypes;
+    }
+
+
+
+
+    public Long getRoomRateId() {
+        return roomRateId;
+    }
+
+    public void setRoomRateId(Long roomRateId) {
+        this.roomRateId = roomRateId;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (roomRateId != null ? roomRateId.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+        // TODO: Warning - this method won't work in the case the roomRateId fields are not set
         if (!(object instanceof RoomRate)) {
             return false;
         }
         RoomRate other = (RoomRate) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.roomRateId == null && other.roomRateId != null) || (this.roomRateId != null && !this.roomRateId.equals(other.roomRateId))) {
             return false;
         }
         return true;
@@ -53,7 +158,7 @@ public class RoomRate implements Serializable {
 
     @Override
     public String toString() {
-        return "entity.RoomRate[ id=" + id + " ]";
+        return "entity.RoomRate[ id=" + roomRateId + " ]";
     }
     
 }
--- HotelReservationSystemLibrary/src/entity/RoomType.java
+++ HotelReservationSystemLibrary/src/entity/RoomType.java
@@ -6,10 +6,15 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -22,6 +27,128 @@ public class RoomType implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long roomTypeId;
+     @Column(length = 32, nullable = false)
+    private String name;
+     @Column(length = 128, nullable = false)
+    private String description;
+      @Column(nullable = false)
+    private int size;
+      @Column(length = 32, nullable = false)
+    private String bed;
+      @Column(length = 32, nullable = false)
+    private String capacity;
+      @Column(length = 128, nullable = false)
+      private String amenities;
+      private Boolean isUsed;
+      private Boolean isEnabled;
+      
+      @OneToMany(mappedBy = "roomType")
+      private List<Room> rooms;
+      
+      @ManyToMany
+      private List<RoomRate> roomRates;
+
+    public RoomType() {
+         rooms = new ArrayList<>();
+
+        roomRates = new ArrayList<>();
+    }
+
+    public RoomType(Long roomTypeId, String name, String description, int size, String bed, String capacity, String amenities, Boolean isUsed, Boolean isEnabled) {
+        this.roomTypeId = roomTypeId;
+        this.name = name;
+        this.description = description;
+        this.size = size;
+        this.bed = bed;
+        this.capacity = capacity;
+        this.amenities = amenities;
+        this.isUsed = isUsed;
+        this.isEnabled = isEnabled;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getBed() {
+        return bed;
+    }
+
+    public void setBed(String bed) {
+        this.bed = bed;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(String amenities) {
+        this.amenities = amenities;
+    }
+
+    public Boolean getIsUsed() {
+        return isUsed;
+    }
+
+    public void setIsUsed(Boolean isUsed) {
+        this.isUsed = isUsed;
+    }
+
+    public Boolean getIsEnabled() {
+        return isEnabled;
+    }
+
+    public void setIsEnabled(Boolean isEnabled) {
+        this.isEnabled = isEnabled;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    public List<RoomRate> getRoomRates() {
+        return roomRates;
+    }
+
+    public void setRoomRates(List<RoomRate> roomRates) {
+        this.roomRates = roomRates;
+    }
+    
+      
+
+      
 
     public Long getRoomTypeId() {
         return roomTypeId;
