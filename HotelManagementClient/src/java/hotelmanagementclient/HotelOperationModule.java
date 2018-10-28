d7404f6396a65ccd32f52116bbdd277e7a7b14dc
Matthea Loo <>
2018-10-28 12:10:50 +0800

    created empty client modules, see comments

---
+++ HotelManagementClient/src/java/hotelmanagementclient/HotelOperationModule.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelmanagementclient;
+
+/**
+ *
+ * @author matthealoo
+ */
+//Diff menu for opman and sales, CRUD room, CRUD room type, CRUD room rate, allocate room, view room allocation report
+public class HotelOperationModule {
+    
+}
---
+++ HotelManagementClient/src/java/hotelmanagementclient/MainApp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelmanagementclient;
+
+/**
+ *
+ * @author matthealoo
+ */
+
+//This is where we'll do login for all the employees and sort out which ones can access the various modules based on their access rights
+public class MainApp {
+    
+}
